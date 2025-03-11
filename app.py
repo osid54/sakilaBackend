f298d40bcf7d6b538cbd9d196eed5e9584349f7d
osid54 <osid.potato@gmail.com>
2025-03-11 15:39:24 -0400

    Landing page actors now gets top 5 rented films

--- app.py
+++ app.py
@@ -18,14 +18,14 @@ cursor = db.cursor(dictionary=True)
 @app.route('/films', methods=['GET'])
 def get_films():
     cursor.execute("""SELECT * FROM sakila.film_list;""")
-    films = cursor.fetchall()
-    return jsonify(films)
+    response = jsonify(cursor.fetchall())
+    return response
 
 @app.route('/customers', methods=['GET'])
 def get_cust():
     cursor.execute("""SELECT customer_id as ID, first_name as "FIRST", last_name as "LAST", email as EMAIL FROM sakila.customer;""")
-    customers = cursor.fetchall()
-    return jsonify(customers)
+    response = jsonify(cursor.fetchall())
+    return response
 
 @app.route('/top5films', methods=['GET'])
 def get_t5f():
@@ -43,22 +43,35 @@ def get_t5f():
         group by i.film_id, c.name, fl.actors
         order by RENTALS desc
         limit 5;""")
-    films = cursor.fetchall()
-    response = jsonify(films)
+    response = jsonify(cursor.fetchall())
     return response
 
 @app.route('/top5actors', methods=['GET'])
 def get_t5a():
-    cursor.execute("""select a.actor_id as ID, a.first_name as FIRST, a.last_name as LAST, count(fa.film_id) as FILMCOUNT, film_info as FILMOGRAPHY from actor as a
+    cursor.execute("""select a.actor_id as ID, a.first_name as FIRST, a.last_name as LAST, count(fa.film_id) as FILMCOUNT from actor as a
         join film_actor as fa
         on a.actor_id = fa.actor_id
-        join actor_info as ai
-        on a.actor_id = ai.actor_id
         group by fa.actor_id
         order by filmCount desc
         limit 5;""")
-    actors = cursor.fetchall()
-    response = jsonify(actors)
+    response = jsonify(cursor.fetchall())
+    return response
+
+@app.route('/actor5films', methods=['GET'])
+def get_a5f():
+    actorID = request.args.get('id')
+    cursor.execute("""select f.title as TITLE from rental as r
+        join inventory as i
+        on r.inventory_id = i.inventory_id
+        join film as f
+        on i.film_id = f.film_id
+        join film_actor as fa
+        on fa.film_id = i.film_id
+        where fa.actor_id = %s
+        group by i.film_id
+        order by count(r.rental_id) desc
+        limit 5;""", (actorID,))
+    response = jsonify(cursor.fetchall())
     return response
 
 if __name__ == '__main__':
